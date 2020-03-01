15fbab25ea37d488149a5d9e7fd3f1d296b6d488
marek <marek559@vp.pl>
2020-03-01 11:57:40 +0100

    bootstrap changes, create DB, models changes

--- affiliate/models.py
+++ affiliate/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
+class Post(models.Model):
+    picture = models.ImageField(upload_to='akhdnsakdn')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
