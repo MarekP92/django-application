a6376563a5991d066f746c3cdd0579ad581242ef
marek <marek559@vp.pl>
2020-03-02 08:52:50 +0100

    changes in template and views

--- affiliate/views.py
+++ affiliate/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from .models import Post
 
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def posts(self):
+        return Post.objects.all()
 
 def home(request):
     return render(request, 'home.html', {'posts': Post.objects.all()})
